b95f4aa9466986797dc99395ea93f30b3465c6d4
gyujin <sssgggjjj4895@gmail.com>
2024-05-16 11:41:19 +0900

    first commit

--- linked_list.cpp
+++ linked_list.cpp
@@ -1,3 +1,19 @@
 #include "linked_list.h"
+#include <iostream>
 
-// TODO: LinkedList 클래스 구현 작성
+// 링크드리스트 생성자
+LinkedList::LinkedList() : head_(nullptr), size_(0){};
+// 링크드리스트 소멸자
+LinkedList::~LinkedList()
+{
+    Node *current = head_;
+    while (current != nullptr)
+    {
+        Node *next = current->next_;
+        delete current;
+        current = next;
+    }
+    void LinkedList::print()
+    {
+        }
+}
